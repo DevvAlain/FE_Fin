c843f7dfb9ac27b166ed1b9d9be6d2e215e2e62c
DylanVu <vuthanhduc181203@gmail.com>
2025-11-14 19:50:51 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, useParams, useSearchParams } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import AuthPage from "./components/AuthPage";
 import LoginPage from "./components/LoginPage";
@@ -9,7 +15,9 @@ function VerifyEmailWrapper() {
   const { token } = useParams<{ token: string }>();
   const [searchParams] = useSearchParams();
   const returnUrl = searchParams.get("returnUrl") || undefined;
-  return <AuthPage type="verify-email" token={token || ""} returnUrl={returnUrl} />;
+  return (
+    <AuthPage type="verify-email" token={token || ""} returnUrl={returnUrl} />
+  );
 }
 
 function ResetPasswordWrapper() {
@@ -23,7 +31,10 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/admin/*" element={<AdminPage />} />
-        <Route path="/reset-password/:token" element={<ResetPasswordWrapper />} />
+        <Route
+          path="/reset-password/:token"
+          element={<ResetPasswordWrapper />}
+        />
         <Route path="/verify-email/:token" element={<VerifyEmailWrapper />} />
         <Route path="/" element={<LandingPage />} />
       </Routes>
